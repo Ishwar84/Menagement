3e2a2d04dc68b4458a6ff17da244c6e6ec0111cb
Ishwar84 <72810957+Ishwar84@users.noreply.github.com>
2024-05-03 19:46:53 +0530

    Add files via upload

---
+++ com/in/CollageMenegmentApplication.java
@@ -0,0 +1,13 @@
+package com.CollageMenegment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageMenegmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageMenegmentApplication.class, args);
+	}
+
+}
